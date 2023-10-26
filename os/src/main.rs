00e467174c2e690d8a7fb496c2e8d48f9bbacbe5
lsh <lsh@lshdeMacBook-Air.local>
2023-10-26 23:06:50 +0800

    freestanding-rust

---
+++ os/src/main.rs
@@ -0,0 +1,14 @@
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+
+// This function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) ->!{
+    loop {}
+}
+// This function is the entry point
+#[no_mangle]
+pub extern "C" fn _start() ->! {
+    loop {}
+}
