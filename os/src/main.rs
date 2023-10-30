0493b9154b4bca350047383946597cc148bd5172
lsh <lsh@lshdeMacBook-Air.local>
2023-10-30 16:43:25 +0800

    finish vgabuffer

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,14 +1,25 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 // This function is called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) ->!{
+fn panic(info: &PanicInfo) ->!{
+    print!("{}", info);
     loop {}
 }
-// This function is the entry point
+
 #[no_mangle]
 pub extern "C" fn _start() ->! {
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(_test: &[&dyn Fn()]) {
     loop {}
 }
