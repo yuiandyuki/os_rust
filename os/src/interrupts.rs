6aab9eab97d4b8619e1e91cf6570864073a5e147
lsh <2091145934@qq.com>
2023-11-07 20:39:19 +0800

    test1

--- os/src/interrupts.rs
+++ os/src/interrupts.rs
@@ -1,4 +1,4 @@
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::{structures::idt::{InterruptDescriptorTable, InterruptStackFrame}, instructions::hlt};
 use crate::{println, gdt};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
@@ -25,6 +25,7 @@ lazy_static!{
             .set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()]
             .set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
 }
@@ -106,4 +107,20 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
+}
+
+use x86_64::structures::idt::PageFaultErrorCode;
+use crate::hlt_loop;
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}",stack_frame);
+    hlt_loop();
 }
--- os/src/main.rs
+++ os/src/main.rs
@@ -6,13 +6,65 @@
 
 use core::panic::PanicInfo;
 use os::println;
+use bootloader::{BootInfo, entry_point};
+
+
+entry_point!(kernel_main);
+
+
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use os::memory;
+    use x86_64::{structures::paging::Page, VirtAddr};
+
     println!("Hello World{}", "!");
 
     os::init();
 
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe {
+        memory::init(phys_mem_offset)
+    };
+    // let addresses = [
+    //     0xb8000,
+    //     0x201008,
+    //     0x0100_0020_1a10,
+    //     boot_info.physical_memory_offset,
+    // ];
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe {
+        memory::init(phys_mem_offset)
+    };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
+
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    // let l4_table = unsafe {
+    //     active_level_4_table(phys_mem_offset)
+    // };
+
+    // for (i, entry) in l4_table.iter().enumerate() {
+    //     if !entry.is_unused() {
+    //         println!("L4 Entry {}: {:?}", i, entry);
+
+    //         let phys = entry.frame().unwrap().start_address();
+    //         let virt = phys.as_u64() + boot_info.physical_memory_offset;
+    //         let ptr = VirtAddr::new(virt).as_mut_ptr();
+    //         let l3_table: &PageTable = unsafe {
+    //             &*ptr
+    //         };
+
+    //         for (i, entry) in l3_table.iter().enumerate() {
+    //             if !entry.is_unused() {
+    //                 println!(" L3 Entry {}: {:?}", i, entry);
+    //             }
+    //         }
+    //     }
+    // }
     // unsafe {
     //     *(0xdeadbeef as *mut u8) = 42;
     // };
@@ -23,6 +75,14 @@ pub extern "C" fn _start() -> ! {
     // }
 
     // stack_overflow();
+    // use x86_64::registers::control::Cr3;
+
+    // let (level_4_page_table, _) = Cr3::read();
+    // println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    
+
+    // let ptr = 0xdeadbeaf as *mut u8;
+    // unsafe {*ptr = 42;}
 
     #[cfg(test)]
     test_main();
