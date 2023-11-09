a7166b16129fd878290707dd3ecd242a5a05ab55
lsh <2091145934@qq.com>
2023-11-09 20:31:43 +0800

    heap

--- os/src/main.rs
+++ os/src/main.rs
@@ -5,8 +5,9 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use os::println;
+use os::{println, memory::BootInfoFrameAllocator};
 use bootloader::{BootInfo, entry_point};
+use x86_64::structures::paging::frame;
 
 
 entry_point!(kernel_main);
@@ -36,13 +37,15 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe {
         memory::init(phys_mem_offset)
     };
-    let mut frame_allocator = memory::EmptyFrameAllocator;
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
 
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    // let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    // memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
     // let l4_table = unsafe {
     //     active_level_4_table(phys_mem_offset)
     // };
--- os/src/memory.rs
+++ os/src/memory.rs
@@ -1,8 +1,45 @@
 use x86_64::{
-    structures::paging::{PageTable, PhysFrame, Mapper, Size4KiB, FrameAllocator, Page},
+    structures::paging::{PageTable, PhysFrame, Mapper, Size4KiB, FrameAllocator, Page, frame},
     VirtAddr,
     PhysAddr,
 };
+
+use bootloader::bootinfo::MemoryMap;
+use bootloader::bootinfo::MemoryRegionType;
+
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
+
+impl BootInfoFrameAllocator {
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        BootInfoFrameAllocator { 
+            memory_map, 
+            next: 0, 
+        }
+    }
+}
+
+impl BootInfoFrameAllocator { 
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        let regions = self.memory_map.iter();
+        let usable_regions = regions
+            .filter(|r| r.region_type == MemoryRegionType::Usable);
+        let addr_ranges = usable_regions
+            .map(|r| r.range.start_addr()..r.range.end_addr());
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    } 
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
+    }
+}
 
 unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
     -> &'static mut PageTable 
@@ -60,21 +97,21 @@ pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static>
     OffsetPageTable::new(level_4_table, physical_memory_offset)
 }
 
-pub fn create_example_mapping(
-    page: Page,
-    mapper: &mut OffsetPageTable,
-    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
-) {
-    use x86_64::structures::paging::PageTableFlags as Flags;
+// pub fn create_example_mapping(
+//     page: Page,
+//     mapper: &mut OffsetPageTable,
+//     frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+// ) {
+//     use x86_64::structures::paging::PageTableFlags as Flags;
 
-    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
-    let flags = Flags::PRESENT | Flags::WRITABLE;
+//     let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+//     let flags = Flags::PRESENT | Flags::WRITABLE;
 
-    let map_to_result = unsafe {
-        mapper.map_to(page, frame, flags, frame_allocator)
-    };
-    map_to_result.expect("map_to failed").flush();
-}
+//     let map_to_result = unsafe {
+//         mapper.map_to(page, frame, flags, frame_allocator)
+//     };
+//     map_to_result.expect("map_to failed").flush();
+// }
 
 pub struct EmptyFrameAllocator;
 
